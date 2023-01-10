dcbef79b9855502de6e04ed5026c28fdf636c311
Robert <78419395+Robert-Wachira@users.noreply.github.com>
2023-01-10 22:43:14 +0300

    'navabar commit' 	new file:   sections/NavItem.jsx 	new file:   sections/Navbar.jsx

---
+++ sections/Navbar.jsx
@@ -0,0 +1,49 @@
+import Link from "next/link";
+import React, { useState } from "react";
+import NavItem from "./NavItem";
+
+const MENU_LIST = [
+  { text: "Home", href: "/" },
+  { text: "Articles", href: "/articles" },
+  { text: "Portfolio", href: "/portfolio" },
+  { text: "Contact", href: "/contact" },
+];
+const Navbar = () => {
+  const [navActive, setNavActive] = useState(null);
+  const [activeIdx, setActiveIdx] = useState(-1);
+
+  return (
+    <header>
+      <nav className={`nav`}>
+        <Link href={"/"}>
+          <a>
+            <h1>Raspel</h1>
+          </a>
+        </Link>
+        <div
+          onClick={() => setNavActive(!navActive)}
+          className={`nav__menu-bar`}
+        >
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className={`${navActive ? "active" : ""} nav__menu-list`}>
+          {MENU_LIST.map((menu, idx) => (
+            <div
+              onClick={() => {
+                setActiveIdx(idx);
+                setNavActive(false);
+              }}
+              key={menu.text}
+            >
+              <NavItem active={activeIdx === idx} {...menu} />
+            </div>
+          ))}
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
