bb4c66dffcea0fef8b1a91ded92ce2440945a8a8
Robert <78419395+Robert-Wachira@users.noreply.github.com>
2023-01-26 23:19:15 +0300

    Changes #4
    	modified:   pages/portfolio/Contact.js

--- pages/portfolio/Contact.js
+++ pages/portfolio/Contact.js
@@ -10,7 +10,7 @@ export default function Contact()
                 <div className="container px-5 py-24 mx-auto">
                     <div className="flex flex-col text-center w-full mb-12">
                         <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Contact Us</h1>
-                        <p className="lg:w-2/3 mx-auto leading-relaxed text-base">W</p>
+                        <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Contact Us</p>
                     </div>
                     <div className="lg:w-1/2 md:w-2/3 mx-auto">
                         <div className="flex flex-wrap -m-2">
