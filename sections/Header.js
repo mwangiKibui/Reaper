b9aed08f1a3f9427d9452d86ea4ff9ea4228051a
Robert <78419395+Robert-Wachira@users.noreply.github.com>
2023-01-27 15:27:36 +0300

    Changes #4
    	modified:   sections/Header.js

--- sections/Header.js
+++ sections/Header.js
@@ -42,20 +42,21 @@ const Header = () =>
     }
     return (
         <header className='border-b border-gray-100 dark:border-gray-700'>
-            <div className='container mx-auto px-6 sm:px-6 py-4s flex justify-between items-center'>
+            <div className='container m-auto px-6 sm:px-6 py-4s flex justify-between items-center'>
                 <span className="self-center whitespace-nowrap text-xl font-semibold pt-5"><Link href='/'>Raspel</Link></span>
+                <div class='text-sm font-medium items-start text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700'>
+                    <nav>
+                        <Link href='/' className={`inline-block p-4 ${router.pathname == "/" ? "text-blue-600 border-b-2 border-blue-600 rounded-t-lg active dark:text-blue-500 dark:border-blue-500" : "border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"}`}>Home</Link>
+                        <Link href='/node.js' className={`inline-block p-4 ${router.pathname == "/node.js" ? "text-blue-600 border-b-2 border-blue-600 rounded-t-lg active dark:text-blue-500 dark:border-blue-500" : "border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"}`}>Node.js</Link>
+                        <Link href='/java' className={`inline-block p-4 ${router.pathname == "/java" ? "text-blue-600 border-b-2 border-blue-600 rounded-t-lg active dark:text-blue-500 dark:border-blue-500" : "border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"}`}>Java</Link>
+                        <Link href='/portfolio' className={`inline-block p-4 ${router.pathname == "/portfolio" ? "text-blue-600 border-b-2 border-blue-600 rounded-t-lg active dark:text-blue-500 dark:border-blue-500" : "border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"}`}>Portfolio</Link>
+                    </nav>
+                </div>
             </div>
             <div class="absolute top-0 right-0 pt-5 h-10 w-16">
                 {renderThemeChanger()}
             </div>
-            <div class='text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700 '>
-                <nav>
-                    <Link href='/' className={`inline-block p-4 ${router.pathname == "/" ? "text-blue-600 border-b-2 border-blue-600 rounded-t-lg active dark:text-blue-500 dark:border-blue-500" : "border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"}`}>Home</Link>
-                    <Link href='/node.js' className={`inline-block p-4 ${router.pathname == "/node.js" ? "text-blue-600 border-b-2 border-blue-600 rounded-t-lg active dark:text-blue-500 dark:border-blue-500" : "border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"}`}>Node.js</Link>
-                    <Link href='/java' className={`inline-block p-4 ${router.pathname == "/java" ? "text-blue-600 border-b-2 border-blue-600 rounded-t-lg active dark:text-blue-500 dark:border-blue-500" : "border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"}`}>Java</Link>
-                    <Link href='/portfolio' className={`inline-block p-4 ${router.pathname == "/portfolio" ? "text-blue-600 border-b-2 border-blue-600 rounded-t-lg active dark:text-blue-500 dark:border-blue-500" : "border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"}`}>Portfolio</Link>
-                </nav>
-            </div>
+
         </header >
     )
 }
