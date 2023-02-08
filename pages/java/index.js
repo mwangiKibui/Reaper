195eb38d1bbda896c0d1732a9c3935a94d642803
Robert <78419395+Robert-Wachira@users.noreply.github.com>
2023-02-08 23:42:00 +0300

    Changes #8 	modified:   pages/java/index.js

--- pages/java/index.js
+++ pages/java/index.js
@@ -15,10 +15,7 @@ export default function Java()
                         <p className="text-xl">You are currently on the Java Articles Page</p>
                     </div>
                     <div className="container mx-auto py-16 h-full flex flex-col justify-center items-center space-y-12">
-                        <Link href="/" className="bg-blue-600 hover:bg-blue-700 text-white px-6 py-3 rounded-md border-2 border-blue-600 hover:border-blue-700 text-lg
-                                sm:text-xl focus:outline-none focus:ring-4 focus:ring-blue-600 focus:ring-opacity-50 whitespace-nowrap">
-                            Return Home
-                        </Link>
+                        List of Articles
                     </div>
                 </div>
             </Layout>
