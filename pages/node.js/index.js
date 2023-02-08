123b1ffb31ca385f755cef94e63351316386d3db
Robert <78419395+Robert-Wachira@users.noreply.github.com>
2023-02-08 23:43:00 +0300

    Changes #8 	modified:   pages/node.js/index.js

--- pages/node.js/index.js
+++ pages/node.js/index.js
@@ -17,7 +17,7 @@ export default function Node()
                     <h1 className="text-center text-2xl font-bold">Node.js Articles</h1>
                 </div>
 
-                <div>
+                <div className="container mx-auto py-16 h-full flex flex-col justify-center items-center space-y-12">
                     <Posts />
                 </div>
 
