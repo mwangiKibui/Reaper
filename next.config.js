fa1b6e38063811650e751a41fa0758b794310d12
Robert <78419395+Robert-Wachira@users.noreply.github.com>
2023-02-09 21:39:43 +0300

    Changes #9 	modified:   next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
-  webpack5: true,
+  reactStrictMode: true,
+  webpack: true,
   webpack: (config) =>
   {
     config.resolve.fallback = { fs: false };
