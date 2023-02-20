d3f213a90c5608c0aae1f9de7f2ae5d6ee217bca
Robert <78419395+Robert-Wachira@users.noreply.github.com>
2023-02-20 16:07:01 +0300

    Changes #17 	modified:   pages/backend/index.js 	modified:   pages/frontend/index.js

--- pages/frontend/index.js
+++ pages/frontend/index.js
@@ -16,7 +16,7 @@ export default function Java()
                             <p className="text-xl leading-7 mt-5">This section outlines various articles that will guide you in implementing various front-end resources that you will be implementing along your path.</p>
                         </div>
                         <div className="container mx-auto py-16 h-full flex flex-col justify-center items-center space-y-12">
-                            List of Articles
+
                         </div>
                     </div>
                 </section>
