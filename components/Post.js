65b88c398d91c703574da207a48e017d86651b1a
Robert <78419395+Robert-Wachira@users.noreply.github.com>
2023-02-20 16:03:41 +0300

    Changes #17 	modified:   components/Post.js 	modified:   pages/backend/index.js 	modified:   pages/frontend/index.js

--- components/Post.js
+++ components/Post.js
@@ -3,9 +3,9 @@ import Link from 'next/link'
 export default function Post({ post })
 {
     return (
-        <div className='shadow-md rounded-lg py-3.5 text-#7f7f7f hover:text-gray-600'>
+        <div className='mt-12 shadow-md rounded-lg py-3.5 text-#7f7f7f hover:text-gray-600 overflow-hidden'>
             <Link href={`/blog/${post.slug}`}>
-                <img src={post.frontmatter.coverImage} alt='' className='object-contain h-48 w-96' />
+                <img src={post.frontmatter.coverImage} alt='' className='h-48 object-cover' />
                 <div className='font-bold text-xl'>{post.frontmatter.title}</div>
                 <div className='mb-2 p-0 text-sm'>{post.frontmatter.date}</div>
                 <p className='mb-2 text-xm'>{post.frontmatter.excerpt}</p>
--- pages/backend/index.js
+++ pages/backend/index.js
@@ -13,8 +13,8 @@ export default function Node()
                 <section className='container mx-auto md:px-20 py-10'>
                     <div className="container mx-auto py-16 h-full flex flex-col space-y-12">
                         <div className="text-center">
-                            <h1 className="text-3xl text-bold">Welcome to the BackEnd Section</h1>
-                            <p className="text-xl mt-5">This section outlines various articles that will guide you in implementing various front-end resources that you will be implementing along your path.</p>
+                            <h1 className="text-4xl leading-10 font-bold tracking-tight">Welcome to the BackEnd Section</h1>
+                            <p className="text-xl leading-7 mt-5">This section outlines various articles that will guide you in implementing various front-end resources that you will be implementing along your path.</p>
                         </div>
                         <div className="container mx-auto py-16 h-full flex flex-col justify-center items-center space-y-12">
                             List of Articles
