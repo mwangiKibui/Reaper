c8e0281de00f134965d4713f6644a089ac63da4c
Robert <78419395+Robert-Wachira@users.noreply.github.com>
2023-02-23 20:42:25 +0300

    Changes #19 	new file:   pages/IT/index.js

---
+++ pages/IT/index.js
@@ -0,0 +1,26 @@
+import Layout from "../../sections/Layout";
+import Link from "next/link"
+
+export default function Java()
+{
+    return (
+        <>
+            <Layout pageMeta={{
+                title: 'FrontEnd',
+                description: 'FrontEnd Articles'
+            }}>
+                <section className='container mx-auto md:px-20 py-10'>
+                    <div className="container mx-auto py-16 h-full flex flex-col space-y-12">
+                        <div className="text-center">
+                            <h1 className="text-4xl leading-10 font-bold tracking-tight">Welcome to the IT Section</h1>
+                            <p className="text-xl leading-7 mt-5">This section outlines various articles that will guide you in implementing various front-end resources that you will be implementing along your path.</p>
+                        </div>
+                        <div className="container mx-auto py-16 h-full flex flex-col justify-center items-center space-y-12">
+
+                        </div>
+                    </div>
+                </section>
+            </Layout>
+        </>
+    )
+}
