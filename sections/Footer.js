87bc622a6673124a073ec9e072be15e8454d6e07
Robert <78419395+Robert-Wachira@users.noreply.github.com>
2023-02-26 10:47:45 +0300

    Changes #21 	modified:   package-lock.json 	modified:   package.json 	modified:   sections/Footer.js

--- sections/Footer.js
+++ sections/Footer.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React from "react"
+import { SocialIcon } from 'react-social-icons'
 
 export default function Footer()
 {
     return (
         <>
-            <footer className="px-4 sm:px-6 py-6 nt-24">
-                <p className="text-center text-sm text-gray-500">
+            <footer className="px-4 sm:px-6 py-6 flex items-start justify-between sticky bottom-0">
+                <div className="flex flex-row items-center">
+                    <SocialIcon
+                        url='https://twitter.com/robawn'
+                        fgColor="gray"
+                        bgColor="transparent"
+                    />
+                    <SocialIcon
+                        url='https://github.com/Robert-Wachira'
+                        fgColor="gray"
+                        bgColor="transparent"
+                    />
+                </div>
+                <p className="items-center justify-center text-xl text-gray-500">
                     © {new Date().getFullYear()} Raspel.
                 </p>
             </footer>
